3445832fcf4227dfc2cae3479ba039e3b6c17d1f
spgray0426 <45480676+spgray0426@users.noreply.github.com>
2025-01-10 19:32:49 +0900

    Revert "Newitem"

--- UC2Team2Project001/ConsumableItem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Item.h"
-
-class ConsumableItem : public Item {
-public:
-    ConsumableItem(const string& name, const string& description, int value);
-};
-
--- UC2Team2Project001/Potion.cpp
+++
@@ -1,5 +0,0 @@
-#include "pch.h"
-#include "Potion.h"
-
-Potion::Potion(const string& name, const string& description, int value)
-	: ConsumableItem(name, description, value) {}
